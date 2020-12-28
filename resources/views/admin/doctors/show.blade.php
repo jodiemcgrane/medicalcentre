c03767c069da8e8f5755a4f135b4fc11981293e2
vagrant <vagrant@homestead>
2020-12-28 17:05:56 +0000

    Inserting modal when deleting a doctor.

--- resources/views/admin/doctors/show.blade.php
+++ resources/views/admin/doctors/show.blade.php
@@ -37,14 +37,36 @@
 
                     <a href="{{ route('admin.doctors.index') }}" class="btn btn-default">Back</a>
                     <a href="{{ route('admin.doctors.edit', $doctor->id) }}" class="btn btn-warning">Edit</a>
-                    <form style="display:inline-block" method="POST" action="{{ route('admin.doctors.destroy', $doctor->id) }}">
+                    <form style="display:inline-block" method="POST" id="delete-form" action="{{ route('admin.doctors.destroy', $doctor->id) }}">
                         <input type="hidden" name="_method" value="DELETE">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                        <button type="submit" class="form-control btn btn-danger">Delete</a>
+                        <button type="submit" class="form-control btn btn-danger" data-toggle="modal" data-target="#delete-modal">Delete</a>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="clearfix"></div>
+    <div class="modal fade" id="delete-modal">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Delete Doctor</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" onclick="document.querySelector('#delete-form').submit()">Proceed</button>
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Cancel</button>
+            </div>
+        </div>
+        </div>
+    </div>
+
 </div>
 @endsection
